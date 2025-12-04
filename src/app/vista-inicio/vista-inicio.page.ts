a9335a73fa04c42aa2cdf45368ec52d1a9f42145
Pedro <pp49653@gmail.com>
2025-12-03 22:37:17 -0300

    actualizo SCSS vista inicio

--- src/app/vista-inicio/vista-inicio.page.ts
+++ src/app/vista-inicio/vista-inicio.page.ts
@@ -2,18 +2,20 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-vista-inicio',
   standalone: true,
-  imports: [IonicModule, CommonModule, RouterModule,TranslateModule ],
+  imports: [IonicModule, CommonModule, RouterModule, TranslateModule],
   templateUrl: './vista-inicio.page.html',
   styleUrls: ['./vista-inicio.page.scss'],
-  
 })
-export class VistaInicioPage {constructor(private translate: TranslateService) {
+export class VistaInicioPage {
+
+  constructor(private translate: TranslateService) {
     const savedLang = localStorage.getItem('lang') || 'es';
     this.translate.setDefaultLang('es');
     this.translate.use(savedLang);
-  }}
+  }
+}
